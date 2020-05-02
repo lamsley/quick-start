b8fc60e2d5cc92e6e022d2be637752bc6b1f8dc7
lamsley <1208645480@qq.com>
2020-05-02 18:46:26 +0800

    new project v0.1

---
+++ src/main/java/lamsley/App.java
@@ -0,0 +1,13 @@
+package lamsley;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!  V0.1" );
+    }
+}
