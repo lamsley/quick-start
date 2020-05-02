ba959986fd79447230a01b894accf4f07bc63c26
lamsley <1208645480@qq.com>
2020-05-03 00:38:28 +0800

    V0.5

--- src/main/java/lamsley/App.java
+++ src/main/java/lamsley/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!  V0.2" );
+        System.out.println( "Hello World!  V0.2 lamsley branch" );
     }
 }
